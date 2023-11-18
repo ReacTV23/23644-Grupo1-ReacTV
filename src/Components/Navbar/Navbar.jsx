0fa1b8092d07f6a6edaa5d2661638faf83ddad55
Horaciomdp <horazopo@gmail.com>
2023-11-18 13:35:26 -0300

    nav bar sin logo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,37 +1,38 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, IconButton, InputBase, Button } from '@mui/material';
+import { AppBar, Toolbar,IconButton, InputBase, Button } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 
 const Navbar = () => {
   return (
-    <AppBar position="static" sx={{ backgroundColor: '#2196f3' }}>
+    <AppBar position="static" sx={{ backgroundColor: '#003686' }}>
       <Toolbar>
-        {/* Logo */}
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        {/* Sección del logo (a la izquierda) */}
+        <div style={{ display: 'flex', alignItems: 'center' }}>
           <img
-            src="tu_logo.png"
+            src="../../../public/assets/img/imagen/logo-reactv.png"
             alt="Logo"
             style={{ height: '40px', marginRight: '16px' }}
           />
-        </Typography>
-
-        {/* Barra de búsqueda */}
-        <div style={{ display: 'flex', alignItems: 'center', marginRight: '16px' }}>
-          <IconButton color="inherit" size="large">
-            <SearchIcon />
-          </IconButton>
-          <InputBase
-            placeholder="Buscar..."
-            inputProps={{ 'aria-label': 'buscar' }}
-            style={{ width: '120px', color: 'white' }}
-          />
         </div>
 
-        {/* Botón de inicio de sesión */}
-        <Button color="inherit" size="large" startIcon={<AccountCircle />} sx={{ color: 'white' }}>
-          Login
-        </Button>
+        {/* Sección de la barra de búsqueda y botón de inicio de sesión (a la derecha) */}
+        <div style={{ display: 'flex', alignItems: 'center', marginLeft: 'auto' }}>
+          <div style={{ display: 'flex', alignItems: 'center', marginRight: '16px' }}>
+            <IconButton color="inherit" size="large">
+              <SearchIcon />
+            </IconButton>
+            <InputBase
+              placeholder="Buscar..."
+              inputProps={{ 'aria-label': 'buscar' }}
+              style={{ width: '200px', color: 'white' }}
+            />
+          </div>
+
+          <Button color="inherit" size="large" startIcon={<AccountCircle />} sx={{ color: '#E08400' }}>
+            Login
+          </Button>
+        </div>
       </Toolbar>
     </AppBar>
   );
