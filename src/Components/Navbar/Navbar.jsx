3f8c823b17663acd865bb5e6f0d3e46d6a7f9a76
Horaciomdp <horazopo@gmail.com>
2023-11-18 12:35:59 -0300

    barra

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { AppBar, Toolbar, Typography, IconButton, InputBase, Button } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <AppBar position="static" sx={{ backgroundColor: '#2196f3' }}>
+      <Toolbar>
+        {/* Logo */}
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <img
+            src="tu_logo.png"
+            alt="Logo"
+            style={{ height: '40px', marginRight: '16px' }}
+          />
+        </Typography>
 
-export default Navbar
+        {/* Barra de búsqueda */}
+        <div style={{ display: 'flex', alignItems: 'center', marginRight: '16px' }}>
+          <IconButton color="inherit" size="large">
+            <SearchIcon />
+          </IconButton>
+          <InputBase
+            placeholder="Buscar..."
+            inputProps={{ 'aria-label': 'buscar' }}
+            style={{ width: '120px', color: 'white' }}
+          />
+        </div>
+
+        {/* Botón de inicio de sesión */}
+        <Button color="inherit" size="large" startIcon={<AccountCircle />} sx={{ color: 'white' }}>
+          Login
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
