d8a3aa1c26b4b883c6e2cb675a01627da07c7771
MoniCrichton <email@example.comcodeando3@gmail.com>
2023-11-18 13:22:40 -0300

    resolvioendo conflicto de Merge con .src/app.js

---
+++ src/components/componente-busqueda/datosSimulados.js
@@ -0,0 +1,19 @@
+// datosSimulados.js
+const datosSimulados = [
+    {
+      id: 1,
+      title: "Pelicula 1",
+      overview: "Descripción de la película 1",
+      poster_path: "/ruta/al/poster1.jpg"
+    },
+    {
+      id: 2,
+      title: "Pelicula 2",
+      overview: "Descripción de la película 2",
+      poster_path: "/ruta/al/poster2.jpg"
+    },
+    // Puedes agregar más objetos según sea necesario
+  ];
+  
+  export default datosSimulados;
+  
