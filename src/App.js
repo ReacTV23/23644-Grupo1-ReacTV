3a601cd3eff471e3c4e1d50430571147e2e1fba2
hymsoft <hymsoft@gmail.com>
2023-11-19 16:49:14 -0300

    Resolucion de conflictos despues del Merge

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
+import CssBaseline from '@mui/material/CssBaseline';
+import Filter from './components/filter/Filter';
+
+import Home from './pages/Home/Home';
+// import Recientes from './pages/Recientes/Recientes';
+// import Categorias from './pages/Categorias/Categorias';
+// import Generos from './pages/Generos/Generos';
+// import MiLista from './pages/MiLista/MiLista';
+// import Lanzamientos from './pages/Lanzamientos/Lanzamientos';
+// import About  from './pages/About/About';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <CssBaseline />
+      <Home />
+      <Filter />
+      {/* <Recientes/> */}
+      {/* <Categorias/> */}
+      {/* <Generos/> */}
+      {/* <MiLista/> */}
+      {/* <Lanzamientos/> */}
+      {/* <About/> */}
+    </>
   );
 
 }
 
 export default App;
-
