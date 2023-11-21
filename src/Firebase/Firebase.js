a6d024da83046c1602c99591f9b410518151245d
Antonela <antonelaborgogno@gmail.com>
2023-11-21 12:27:03 -0300

    descomentado de archivo de firebase

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -1,20 +1,20 @@
-// // Import the functions you need from the SDKs you need
-// import {initializeApp} from "firebase/app";
-// import {getAuth} from 'firebase/auth';
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+import {getAuth} from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_API_KEY,
-//   authDomain: process.env.REACT_APP_API_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_API_PROJECT_ID ,
-//   storageBucket: process.env.REACT_APP_API_STORAGE_BUCKET,
-//   messagingSenderId:process.env.REACT_APP_API_MESSAGING_SENDERID,
-//   appId: process.env.REACT_APP_API_ID,
-// };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_API_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_API_PROJECT_ID ,
+    storageBucket: process.env.REACT_APP_API_STORAGE_BUCKET,
+    messagingSenderId:process.env.REACT_APP_API_MESSAGING_SENDERID,
+    appId: process.env.REACT_APP_API_ID,
+};
 
-// // Initialize Firebase
-// // console.log("Firebase Config:", firebaseConfig);
-// export const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app)
+// Initialize Firebase
+// console.log("Firebase Config:", firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
