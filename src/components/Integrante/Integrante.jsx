81008b6c531b98e075967d84e140d693326c10ea
Horaciomdp <horazopo@gmail.com>
2023-11-22 11:14:17 -0300

    firebase con integrantes finalizado

--- src/components/Integrante/Integrante.jsx
+++ src/components/Integrante/Integrante.jsx
@@ -6,14 +6,18 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import { ButtonGroup } from 'react-bootstrap';
 import './Integrante.css';
 
-const Integrante = ({nombre, detalles}) => {
+const Integrante = ({nombre,apellido, Github, Linkedin}) => {
   return (
     <div className='Contenedor' key={nombre}>
-      <div className='Nombre'>{nombre}</div>
+      <div className='Nombre'>{nombre} {apellido}</div>
       <ButtonGroup className='GrupoBotones'
         aria-label="small button group">
+        <a href={Github} target="_blank" rel="noopener noreferrer">
           <Boton Contenido={GitHubIcon}/>
+        </a>
+        <a href={Linkedin} target="_blank" rel="noopener noreferrer">
           <Boton Contenido={LinkedInIcon}/>
+        </a>
     </ButtonGroup>
     </div> 
 
