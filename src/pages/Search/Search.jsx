40ad4330822ad6d45af113e3c1bda2ca26ccab0f
Antonela <antonelaborgogno@gmail.com>
2023-11-26 01:55:21 -0300

    borrado boton limpieza

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import LayoutSecundario from '../../layout/LayoutSecundario/LayoutSecundario'
 import MoviesContainer from '../../components/Busqueda/MoviesContainer'
-import { useSearch } from '../../context/searchContext';  // Importa el hook useSearch
 
 const Search = () => {
-    const { clearSearchQuery } = useSearch(); // Obtiene el estado y las funciones de búsqueda del contexto
     return (
         <LayoutSecundario textoBoton={'resultados'}>
             <MoviesContainer/>
-            <button onClick={clearSearchQuery}>Limpiar búsqueda</button>
         </LayoutSecundario>
     )
 }
