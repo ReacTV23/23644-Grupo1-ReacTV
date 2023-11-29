9879a42142ba07fe87fa6ecfdef61705e26b00e8
Antonela <antonelaborgogno@gmail.com>
2023-11-28 23:19:33 -0300

    cambios en Calendar

--- src/Components/Calendar/Calendar.jsx
+++ src/Components/Calendar/Calendar.jsx
@@ -6,8 +6,8 @@ import EventIcon from '@mui/icons-material/Event';
 import CardImg from '../Card/CardImg/CardImg';
 import Boton from '../Boton';
 import { getUpcomingMovies } from '../../services/tmdbService.js';
-import 'react-calendar/dist/Calendar.css';
 import { useNavigate } from 'react-router-dom';
+import 'react-calendar/dist/Calendar.css';
 import './Calendar.css';
 
 const WritableCalendar = ({ onInfoChange }) => {
@@ -15,12 +15,12 @@ const WritableCalendar = ({ onInfoChange }) => {
   const [date, setDate] = useState(new Date());
   const [events, setEvents] = useState({});
   const [upcomingMovies, setUpcomingMovies] = useState([]);
-  const overlayRef = useRef(null);
+  // const overlayRef = useRef(null);
   const [selectedMovieId, setSelectedMovieId] = useState(null);
+  // const [selectedPoster, setSelectedPoster] = useState(null);
+  const calendarContainerRef = useRef(null);
 
-  useEffect(() => {
-    getUpcomingMoviesData();
-  }, []);
+  const IMAGE_PATH = process.env.REACT_APP_URL_IMAGE_TMDB;
 
   const getUpcomingMoviesData = async () => {
     try {
@@ -31,6 +31,10 @@ const WritableCalendar = ({ onInfoChange }) => {
     }
   };
 
+  useEffect(() => {
+    getUpcomingMoviesData();
+  }, []);
+
   const handleDateChange = (newDate) => {
     setDate(newDate);
   };
@@ -38,76 +42,238 @@ const WritableCalendar = ({ onInfoChange }) => {
   const handleMovieClick = (movieId) => {
     console.log('Clicked on movie with ID:', movieId);
     setSelectedMovieId(movieId);
+    // setSelectedPoster(true)
   };
+
+  // const handlePosterClick = ({ peli, imageDataUrl }) => {
+  //   setSelectedPoster({ peli, imageDataUrl });
+  // };
 
   const handleClick = (peli) => {
     navigate('/card', { dato: peli });
   };
 
-  const downloadCalendar = async () => {
-    try {
-      const calendarContainer = document.getElementById('calendar-container');
-      const overlayContainer = overlayRef.current;
 
-      const wrapperElement = document.createElement('div');
-      wrapperElement.className = 'calendar-wrapper';
+  // Antes de la función downloadCalendar, descarga las imágenes localmente
+const downloadImagesLocally = async () => {
+  const promises = upcomingMovies.map(async (movie) => {
+    const response = await fetch(`${IMAGE_PATH + movie.poster_path}`);
+    const blob = await response.blob();
+    const objectURL = URL.createObjectURL(blob);
+    return { id: movie.id, imageDataUrl: objectURL };
+  });
 
-      const clonedCalendar = calendarContainer.cloneNode(true);
-      wrapperElement.appendChild(clonedCalendar);
+  const imageArray = await Promise.all(promises);
+  const imageMap = imageArray.reduce((map, item) => {
+    map[item.id] = item.imageDataUrl;
+    return map;
+  }, {});
 
-      const eventsDiv = document.createElement('div');
-      eventsDiv.className = 'events-overlay';
-      wrapperElement.appendChild(eventsDiv);
+  return imageMap;
+};
 
-      Object.entries(events).forEach(([eventDate, eventText]) => {
-        const dateCell = clonedCalendar.querySelector(
-          `.react-calendar__tile[data-date="${eventDate.split('T')[0]}"]`
-        );
-        if (dateCell) {
-          const eventDiv = document.createElement('div');
-          eventDiv.className = 'event-marker';
-          eventDiv.textContent = eventText;
-          eventsDiv.appendChild(eventDiv);
-        }
-      });
 
-      upcomingMovies.forEach((movie) => {
-        const movieDate = new Date(movie.release_date);
-        const dateString = movieDate.toISOString().split('T')[0];
-        const dateCell = clonedCalendar.querySelector(
-          `.react-calendar__tile[data-date="${dateString}"]`
-        );
-        if (dateCell) {
-          const movieDiv = document.createElement('div');
-          movieDiv.className = 'movie-marker';
-          movieDiv.textContent = movie.title;
-          movieDiv.setAttribute('data-id', movie.id);
-          eventsDiv.appendChild(movieDiv);
-        }
-      });
+const downloadCalendar = async () => {
+  try {
+    const calendarContainer = calendarContainerRef.current;
 
-      window.requestAnimationFrame(async () => {
-        const canvas = await html2canvas(document.documentElement, {
-          scale: 2,
-          allowTaint: true,
-          useCORS: false,
-        });
-
-        const imageDataUrl = canvas.toDataURL('image/jpeg', 1.0);
-
-        const a = document.createElement('a');
-        a.href = imageDataUrl;
-        a.download = 'full_document.jpg';
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-
-        overlayContainer.innerHTML = '';
-      });
-    } catch (error) {
-      console.error('Error capturing document:', error);
+    if (!calendarContainer) {
+      console.error('Calendario no encontrado');
+      return;
     }
-  };
+
+    // Descargar las imágenes localmente
+    const images = await downloadImagesLocally();
+
+    // Capturar el contenido del calendario
+    const canvas = await html2canvas(calendarContainer, {
+      scale: 1,
+      allowTaint: true,
+      useCORS: false,
+    });
+
+    const context = canvas.getContext('2d');
+
+    // Utilizar las imágenes locales al dibujar el contenido en el lienzo
+    upcomingMovies.forEach((movie) => {
+      const imageDataUrl = images[movie.id];
+      const movieDate = new Date(movie.release_date);
+      const dateString = movieDate.toISOString().split('T')[0];
+      const dateCell = calendarContainer.querySelector(
+        `.react-calendar__tile[data-date="${dateString}"]`
+      );
+
+      if (dateCell) {
+        // Dibujar la imagen del póster en el lienzo
+        const img = new Image();
+        img.src = imageDataUrl;
+        context.drawImage(img, 0, 0,  1200, 600);
+      }
+    });
+
+    // Convertir el contenido capturado a una URL de datos
+    const imageDataUrl = canvas.toDataURL('image/jpeg', 1.0);
+
+    // Crear un enlace para descargar la imagen
+    const a = document.createElement('a');
+    a.href = imageDataUrl;
+    a.download = 'full_calendar.jpg';
+    document.body.appendChild(a);
+
+    // Simular un clic en el enlace para iniciar la descarga
+    a.click();
+
+    // Eliminar el enlace después de la descarga
+    document.body.removeChild(a);
+  } catch (error) {
+    console.error('Error capturing calendar:', error);
+  }
+};
+
+
+  // const downloadCalendar = async () => {
+  //   try {
+  //     const calendarContainer = document.getElementById('contenedor');
+  //     const overlayContainer = overlayRef.current;
+  
+  //     const eventsDiv = document.createElement('div');
+  //     eventsDiv.className = 'events-overlay';
+  
+  //     Object.entries(events).forEach(([eventDate, eventText]) => {
+  //       const dateCell = calendarContainer.querySelector(
+  //         `.react-calendar__tile[data-date="${eventDate.split('T')[0]}"]`
+  //       );
+  //       if (dateCell) {
+  //         const eventDiv = document.createElement('div');
+  //         eventDiv.className = 'event-marker';
+  //         eventDiv.textContent = eventText;
+  //         eventsDiv.appendChild(eventDiv);
+  //       }
+  //     });
+  
+  //     // Lógica para procesar el póster seleccionado
+  //     if (selectedPoster) {
+  //       const posterDiv = document.createElement('div');
+  //       posterDiv.className = 'selected-poster';
+  //       const posterImg = document.createElement('img');
+  //       posterImg.src = selectedPoster.imageDataUrl;  // Asegúrate de que selectedPoster esté definido
+  //       posterDiv.appendChild(posterImg);
+  //       overlayContainer.appendChild(posterDiv);
+  //     }
+  
+  //     upcomingMovies.forEach((movie) => {
+  //       const movieDate = new Date(movie.release_date);
+  //       const dateString = movieDate.toISOString().split('T')[0];
+  //       const dateCell = calendarContainer.querySelector(
+  //         `.react-calendar__tile[data-date="${dateString}"]`
+  //       );
+  //       if (dateCell) {
+  //         const movieDiv = document.createElement('div');
+  //         movieDiv.className = 'movie-marker';
+  //         const cardImgDiv = document.createElement('div');
+  //         cardImgDiv.appendChild(
+  //           <CardImg
+  //             peli={upcomingMovies.find((m) => m.id === movie.id)}
+  //             onPosterClick={handlePosterClick}
+  //             funcion={() => handleClick(movie)}
+  //           />
+  //         );
+  //         movieDiv.appendChild(cardImgDiv);
+  //         eventsDiv.appendChild(movieDiv);
+  //       }
+  //     });
+  
+  //     overlayContainer.appendChild(eventsDiv);
+  
+  //     window.requestAnimationFrame(async () => {
+  //       const canvas = await html2canvas(document.documentElement, {
+  //         scale: 1,
+  //         allowTaint: true,
+  //         useCORS: false,
+  //       });
+  
+  //       const imageDataUrl = canvas.toDataURL('image/jpeg', 1.0);
+  
+  //       const a = document.createElement('a');
+  //       a.href = imageDataUrl;
+  //       a.download = 'full_document.jpg';
+  //       document.body.appendChild(a);
+  //       a.click();
+  //       document.body.removeChild(a);
+  
+  //       overlayContainer.innerHTML = '';
+  //     });
+  //   } catch (error) {
+  //     console.error('Error capturing document:', error);
+  //   }
+  // };
+  
+
+  // const downloadCalendar = async () => {
+  //   try {
+  //     const calendarContainer = document.getElementById('contenedor');
+  //     const overlayContainer = overlayRef.current;
+
+  //     const wrapperElement = document.createElement('div');
+  //     wrapperElement.className = 'calendar-wrapper';
+
+  //     const clonedCalendar = calendarContainer.cloneNode(true);
+  //     wrapperElement.appendChild(clonedCalendar);
+
+  //     const eventsDiv = document.createElement('div');
+  //     eventsDiv.className = 'events-overlay';
+  //     wrapperElement.appendChild(eventsDiv);
+
+  //     Object.entries(events).forEach(([eventDate, eventText]) => {
+  //       const dateCell = clonedCalendar.querySelector(
+  //         `.react-calendar__tile[data-date="${eventDate.split('T')[0]}"]`
+  //       );
+  //       if (dateCell) {
+  //         const eventDiv = document.createElement('div');
+  //         eventDiv.className = 'event-marker';
+  //         eventDiv.textContent = eventText;
+  //         eventsDiv.appendChild(eventDiv);
+  //       }
+  //     });
+
+  //     upcomingMovies.forEach((movie) => {
+  //       const movieDate = new Date(movie.release_date);
+  //       const dateString = movieDate.toISOString().split('T')[0];
+  //       const dateCell = clonedCalendar.querySelector(
+  //         `.react-calendar__tile[data-date="${dateString}"]`
+  //       );
+  //       if (dateCell) {
+  //         const movieDiv = document.createElement('div');
+  //         movieDiv.className = 'movie-marker';
+  //         movieDiv.textContent = movie.title;
+  //         movieDiv.setAttribute('data-id', movie.id);
+  //         eventsDiv.appendChild(movieDiv);
+  //       }
+  //     });
+
+  //     window.requestAnimationFrame(async () => {
+
+  //       const canvas = await html2canvas(document.documentElement, {
+  //         scale: 2,
+  //         allowTaint: true,
+  //         useCORS: false,
+  //       });
+
+  //       const imageDataUrl = canvas.toDataURL('image/jpeg', 1.0);
+
+  //       const a = document.createElement('a');
+  //       a.href = imageDataUrl;
+  //       a.download = 'full_document.jpg';
+  //       document.body.appendChild(a);
+  //       a.click();
+  //       document.body.removeChild(a);
+
+  //       overlayContainer.innerHTML = '';
+  //     });
+  //   } catch (error) {
+  //     console.error('Error capturing document:', error);
+  //   }
+  // };
 
   const tileContent = ({ date, view }) => {
     if (view === 'month') {
@@ -143,9 +309,10 @@ const WritableCalendar = ({ onInfoChange }) => {
   };
 
   return (
-    <div className="container mt-2" style={{ width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+    <div className="container mt-2"  style={{ width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+      <div ref={calendarContainerRef}>
       <h5 className="mb-2" style={{ textAlign: 'center', textTransform: 'uppercase' }}>Calendario de estrenos</h5>
-      <div className="row" style={{ width: '100%', display: 'flex' }}>
+      <div className="row"  style={{ width: '100%', display: 'flex' }}>
         <div className="card p-3" id="calendar-container" style={{ width: '60%', backgroundColor: '#003686' }}>
           <Calendar
             onChange={handleDateChange}
@@ -169,11 +336,12 @@ const WritableCalendar = ({ onInfoChange }) => {
           </div>
         )}
       </div>
+      </div>
       <div className="mt-3">
         <Boton Contenido={DownloadForOfflineIcon} color={'#003686'} colorHover={'#E08400'} fontSize={'60px'} funcion={downloadCalendar} />
         <Boton Contenido={EventIcon} color={'#003686'} colorHover={'#E08400'} fontSize={'60px'} />
       </div>
-      <div ref={overlayRef} className="overlay-container" style={{ display: 'none' }} />
+      {/* <div ref={overlayRef} className="overlay-container" style={{ display: 'none' }} /> */}
     </div>
   );
 };
