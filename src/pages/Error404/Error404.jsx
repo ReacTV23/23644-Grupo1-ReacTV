2a449cdaa66ce942dc88114e64135440dc0952ab
Fani Casco <fanymar@hotmail.com>
2023-11-30 15:02:08 -0300

    Creacion de componente Error404

---
+++ src/pages/Error404/Error404.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+//import NotFound from '../../components/Error404/NotFound';
+
+function Error404() {
+    return (
+        <div>
+          {/*<NotFound/>*/}  
+        </div>
+    );
+}
+
+export default Error404;
+
