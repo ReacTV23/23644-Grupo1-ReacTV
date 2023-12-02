9f5b21851c86a918d22363965b438bc9b4991f48
Antonela <antonelaborgogno@gmail.com>
2023-12-01 23:40:50 -0300

    cambios en ruta de MovieCard para tipo y id, arreglo de mayusuclas en Contacto y layout en Error404

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import NotFound from '../../components/Error404/NotFound';
+import LayoutMain from '../../layout/LayoutMain/LayoutMain'
 
 function Error404() {
     return (
-
-<NotFound/>
-
-       
+        <LayoutMain>
+            <NotFound/>
+        </LayoutMain>
     );
 }
 
 export default Error404;
-
